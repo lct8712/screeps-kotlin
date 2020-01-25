3a244838f939a116fc46e189b4c3bf6764eb0522
chentian <lct8712@gmail.com>
2020-01-25 22:32:00 +0800

    link

--- src/main/kotlin/chentian/utils/LinkUtils.kt
+++ src/main/kotlin/chentian/utils/LinkUtils.kt
@@ -19,9 +19,10 @@ class RoomLinkInfo(
 )
 
 val TARGET_ROOM_LINK = listOf(
-//    RoomLinkInfo("E18S18", "5cdad0cdf9cba63e6c385dfd", "5cdacbb4e470435ac71db0cf"),
-//    RoomLinkInfo("E18S18", "5d2071da14a08a72e00d134c", "5cdacbb4e470435ac71db0cf")
-    RoomLinkInfo("E17S17", "5d43c038d16c4b73af5acfa0", "5d44004ccb3b537470a4633e")
+    RoomLinkInfo("E18S18", "5cdad0cdf9cba63e6c385dfd", "5cdacbb4e470435ac71db0cf"),
+    RoomLinkInfo("E18S18", "5d2071da14a08a72e00d134c", "5cdacbb4e470435ac71db0cf"),
+    RoomLinkInfo("E17S17", "5d43c038d16c4b73af5acfa0", "5d44004ccb3b537470a4633e"),
+    RoomLinkInfo("E17S17", "5e2c3ab6ece8182cd4d219eb", "5d44004ccb3b537470a4633e")
 )
 
 fun linkTransfer() {
