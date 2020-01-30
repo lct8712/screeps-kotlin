401bbf5adee4c997b84bd8ba67a69e19d667e5f0
chentian <lct8712@gmail.com>
2020-01-30 20:12:04 +0800

    runSellEnergyAndResource

--- src/main/kotlin/chentian/strategy/CreepStrategyHarvesterResource.kt
+++ src/main/kotlin/chentian/strategy/CreepStrategyHarvesterResource.kt
@@ -1,13 +1,16 @@
 package chentian.strategy
 
+import chentian.extensions.controlLevel
 import chentian.extensions.findCreepByRole
 import chentian.extensions.transferAllTypeOrMove
 import chentian.utils.createNormalCreep
 import chentian.utils.harvestResourceAndDoJob
 import screeps.api.Creep
 import screeps.api.FIND_MINERALS
+import screeps.api.FIND_MY_STRUCTURES
 import screeps.api.FIND_STRUCTURES
 import screeps.api.Room
+import screeps.api.STRUCTURE_EXTRACTOR
 import screeps.api.STRUCTURE_TERMINAL
 import screeps.api.structures.StructureSpawn
 import screeps.api.structures.StructureTerminal
@@ -20,7 +23,6 @@ import screeps.api.structures.StructureTerminal
 class CreepStrategyHarvesterResource(val room: Room) : CreepStrategy {
 
     private val creeps = room.findCreepByRole(CREEP_ROLE_HARVESTER_RESOURCE)
-    private val miners = room.find(FIND_MINERALS).filter { it.mineralAmount > 0 }
 
     override fun tryToCreate(spawn: StructureSpawn) {
         if (shouldCreate()) {
@@ -33,7 +35,13 @@ class CreepStrategyHarvesterResource(val room: Room) : CreepStrategy {
     }
 
     private fun shouldCreate(): Boolean {
-        return creeps.size < MAX_CREEP_COUNT_PER_ROOM && miners.isNotEmpty()
+        return creeps.size < MAX_CREEP_COUNT_PER_ROOM &&
+            room.controlLevel() >= 6 &&
+            room.find(FIND_MINERALS).any { it.mineralAmount > 0 } &&
+            room.find(FIND_MY_STRUCTURES).any { it.structureType == STRUCTURE_EXTRACTOR } &&
+            room.find(FIND_MY_STRUCTURES).any {
+                it.structureType == STRUCTURE_TERMINAL && (it as StructureTerminal).store.getFreeCapacity() != 0
+            }
     }
 
     private fun fillEnergy(creep: Creep) {
